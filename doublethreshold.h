e9e5703819be74f8d8a1f15339ed6b4c1d0e39d2
Matthew Altmann <altma094@umn.edu>
2021-10-18 12:27:07 -0500

    Updating doublethreshold

--- doublethreshold.h
+++ doublethreshold.h
@@ -6,8 +6,12 @@
 class DoubleThresholdFilter : public SimpleFilter {
 public:
     DoubleThresholdFilter();
+    
+    DoubleThresholdFilter(float t1, float t2);
 
     unsigned char* applyAtPixel(unsigned char* pixel);
+   
+    float GetLuminance(float red,float green,float blue);
 
     float threshold1;
     float threshold2;
