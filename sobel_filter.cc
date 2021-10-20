c6f2f65d9878a427cb0e206bd549ec5647b6aecc
Emily Iverson <ivers743@umn.edu>
2021-10-20 09:39:53 -0500

    fixed sobel_filter.cc styling

--- sobel_filter.cc
+++ sobel_filter.cc
@@ -1,8 +1,8 @@
 #include "sobel_filter.h"
 
 SobelFilter::SobelFilter(){
-    std::vector<std::vector<float>> kernalPrototype = {{-1,0,1},{-2,0,2},{-1,0,1}};
-    colonel = Kernal(kernalPrototype);
+    std::vector<std::vector<float>> kernalPrototype = {{-1,0,1},{-2,0,2},{-1,0,1}};//x kernal
+    colonel = Kernal(kernalPrototype);//set colonel to x kernal
 }
 
 int min(int a,int b){
@@ -11,7 +11,7 @@ int min(int a,int b){
     }
     return b;
 }
-int max(int a,int b){
+int max(int a,int b){ //returns the maximum of 2 numbers
     if(a>b){
         return a;
     }
@@ -19,84 +19,74 @@ int max(int a,int b){
 }
 
 void SobelFilter::Apply(std::vector<Image*> input, std::vector<Image*> output){
-    Image *intensity = output.at(0);
-    Image *direction =output.at(1);
 
-    Image *in = input.at(0);
+    Image *intensity = output.at(0);//intensity image
+    Image *direction =output.at(1);//direction image
 
-    *intensity = *in;
-    *direction = *in;
+    Image *in = input.at(0);//input image
 
-    std::vector<std::vector<float>> kernalPrototype = {{1,2,1},{0,0,0},{-1,-2,-1}};
-    Kernal Kernal_y = Kernal(kernalPrototype);
+    *intensity = *in;//sit width and height to input image
+    *direction = *in;//sit width and height to input image
+
+    std::vector<std::vector<float>> kernalPrototype = {{1,2,1},{0,0,0},{-1,-2,-1}};//y kernal
+    Kernal Kernal_y = Kernal(kernalPrototype);//set y kernal
 
 
     for(int y=0; y<in->GetHeight(); y++){
-        for(int x=0; x<in->GetWidth(); x++){
-            int red_x=0;
-            int green_x=0;
-            int blue_x=0;
+        for(int x=0; x<in->GetWidth(); x++){//iterate through image
+            int lum_x=0;//luminance of the x pixel
+            int lum_y=0;//luminance of the y pixel
 
-            int red_y=0;
-            int green_y=0;
-            int blue_y=0;
+            int size = colonel.getSize();//kernal size
 
-            int size = colonel.getSize();
             for(int i = 0;i<size;i++){
-                for(int j = 0;j<size;j++){
-                    unsigned char* pixel = in->GetPixel(x+(i-floor(size/2)),y+(j-floor(size/2)));
-                    red_x+= pixel[0]*colonel.get(i,j);
-                    green_x+= pixel[1]*colonel.get(i,j);
-                    blue_x+=pixel[2]*colonel.get(i,j);
+                for(int j = 0;j<size;j++){//iterate through the kernals
 
-                    red_y += pixel[0]*Kernal_y.get(i,j);
-                    green_y += pixel[1]*Kernal_y.get(i,j);
-                    blue_y += pixel[2]*Kernal_y.get(i,j);
+                    unsigned char* pixel = in->GetPixel(x+(i-floor(size/2)),y+(j-floor(size/2)));//get pixel
+
+                    lum_x += pixel[0]*colonel.get(i,j);//get x luminance
+
+                    lum_y += pixel[0]*Kernal_y.get(i,j);//get y luminance
                 }
             }
 
+            unsigned char* newPixel_i= new unsigned char[4];//new pixel for intensity image
 
-            // std::cout<<red_x<<" "<<green_x<<" "<<blue_x<<std::endl<<red_y<<" "<<green_y<<" "<<blue_y<<std::endl<<std::endl;
+            int intensityLevel = min(floor(sqrt(lum_x*lum_x+lum_y*lum_y)),255);//get intensity level
 
-            unsigned char* newPixel_i= new unsigned char[4];
-
-            newPixel_i[0] = min(floor(sqrt(red_x*red_x+red_y*red_y)),255);
-            newPixel_i[1] = min(floor(sqrt(green_x*green_x+green_y*green_y)),255);
-            newPixel_i[2] = min(floor(sqrt(blue_x*blue_x+blue_y*blue_y)),255);
+            //set the pixel
+            newPixel_i[0] = intensityLevel;
+            newPixel_i[1] = intensityLevel;
+            newPixel_i[2] = intensityLevel;
             newPixel_i[3] = 255;
 
-            // std::cout<<min(floor(sqrt(pow(red_x,2)+pow(red_y,2))),255)<<" "<<min(floor(sqrt(pow(green_x,2)+pow(green_y,2))),255)<<" "<<min(floor(sqrt(pow(blue_x,2)+pow(blue_y,2))),255)<<std::endl;
 
-            intensity->SetPixel(x,y,newPixel_i);
+            intensity->SetPixel(x,y,newPixel_i);//put pixel in image
 
-            delete[] newPixel_i;
+            delete[] newPixel_i;//delete so no mem leaks
 
-            unsigned char* newPixel_d= new unsigned char[4];
+            unsigned char* newPixel_d= new unsigned char[4];//new pixel for direction matrix
 
-
-
-            // std::cout<< floor(atan2(red_y,red_x)*180/3.1415926535)<<" "<< floor(atan2(green_y,green_x)*180/3.1415926535)<<" "<< floor(atan2(blue_y,blue_x)*180/3.1415926535)<<std::endl;
-
-            if(atan2(red_y,red_x)==0){
+            if(atan2(lum_y,lum_x)==0){//set 0 direction to 0;
                 newPixel_d[0]=0;
                 newPixel_d[1]=0;
                 newPixel_d[2]=0;
             }
             else{
-                float red = atan2(red_y,red_x)+M_PI;
-                // red = abs(red);
-                red*=20;
-                // std::cout<<red<<std::endl;
-                newPixel_d[0] = red;
-                newPixel_d[1] = red;
-                newPixel_d[2] = red;
+                float directionLevel = atan2(lum_y,lum_x)+M_PI;//make sure directionLevel is positive
+                directionLevel*=20;//multiply so more directions for non max suppression
+
+                //set pixel for direction level
+                newPixel_d[0] = directionLevel;
+                newPixel_d[1] = directionLevel;
+                newPixel_d[2] = directionLevel;
             }
 
             newPixel_d[3] = 255;
 
-            direction->SetPixel(x,y,newPixel_d);
+            direction->SetPixel(x,y,newPixel_d);//put pixel in direction image
 
-            delete[] newPixel_d;
+            delete[] newPixel_d;//delete so no mem leaks
         }
     }
 }
