ba52c67c79b0e9267ce3b563956907295ec8c74f
Emily Iverson <ivers743@umn.edu>
2021-12-02 18:11:18 -0600

    edited tests

--- web_app_stuff/tests/4_FactoryUnitTests.cc
+++ web_app_stuff/tests/4_FactoryUnitTests.cc
@@ -50,24 +50,6 @@ protected:
   DroneFactory droneFactory;
 };
 
-class CompositeFactoryTest : public ::testing::Test {
-public:
-  void SetUp( ) {
-    compositeEntityFactory=CompositeEntityFactory();
-    drone_json= "{\"cameras\":[0],\"charge\":20,\"command\":\"createEntity\",\"direction\":[0,0,0],\"duration\":2,\"entityId\":0,\"id\":1,\"name\":\"drone\",\"offset\":[0,0.59999999999999998,0],\"path\":\"drone.glb\",\"position\":[0,2.5,0],\"radius\":1,\"rotation\":[0,0,0],\"scale\":[0.14999999999999999,0.14999999999999999,0.14999999999999999],\"speed\":4,\"start\":2,\"type\":\"actor\"}";
-    robot_json = "{\"cameras\":[3],\"command\":\"createEntity\",\"direction\":[1,0,0],\"entityId\":3,\"id\":2799,\"name\":\"robot\",\"path\":\"robot.glb\",\"position\":[56.139513282630787,0,3.3961168294766111],\"radius\":1,\"rotation\":[0,0,0],\"scale\":[0.25,0.25,0.25],\"type\":\"actee\"}";
-    hospital_json="{\"cameras\":[1],\"command\":\"createEntity\",\"direction\":[1,0,0],\"entityId\":1,\"id\":3,\"name\":\"hospital\",\"path\":\"hospital.glb\",\"position\":[-40,0,2],\"radius\":1,\"rotation\":[0,0,0],\"scale\":[1,1,1],\"type\":\"destination\"}";
-    recharge_json ="{\"cameras\":[2],\"command\":\"createEntity\",\"direction\":[1,0,0],\"entityId\":2,\"id\":3,\"name\":\"recharge station\",\"path\":\"charger.glb\",\"position\":[50,0,20],\"radius\":1,\"rotation\":[0,0,0],\"scale\":[1,1,1],\"type\":\"charger\"}";
-
-  }
-protected:
-  std::string drone_json;
-  std::string robot_json;
-  std::string hospital_json;
-  std::string recharge_json;
-  CompositeEntityFactory compositeEntityFactory;
-};
-
 TEST_F(HospitalFactoryTest, CreateHospital) {
   picojson::value v;
 
@@ -180,4 +162,43 @@ TEST_F(RechargeFactoryTest, CreateRechargeStation) {
   EXPECT_EQ(actualDir.GetZ(),expectedDir.GetZ());
 
   EXPECT_EQ(station->GetId(),2);
+}
+
+TEST_F(DroneFactoryTest, CreateDrone) {
+  picojson::value v;
+
+  std::string err = picojson::parse(v, drone_json);
+  picojson::object o;
+  if (! err.empty()) {
+    std::cerr << err << std::endl;
+    EXPECT_TRUE(false);
+    return;
+  }
+
+  if (v.is<picojson::object>()) {
+     o = v.get<picojson::object>();
+  }
+  else{
+    std::cerr<<"Not an object"<<std::endl;
+    EXPECT_TRUE(false);
+    return;
+  }
+
+  Entity *drone = droneFactory.create(o);
+
+  Point3 expectedPos = Point3(50,0,20);
+  Point3 actualPos = drone->GetPosition();
+  EXPECT_EQ(actualPos.GetX(),expectedPos.GetX());
+  EXPECT_EQ(actualPos.GetY(),expectedPos.GetY());
+  EXPECT_EQ(actualPos.GetZ(),expectedPos.GetZ());
+
+  Vector3 expectedDir = Vector3(1,0,0);
+  Vector3 actualDir = drone->GetDirection();
+  EXPECT_EQ(actualDir.GetX(),expectedDir.GetX());
+  EXPECT_EQ(actualDir.GetY(),expectedDir.GetY());
+  EXPECT_EQ(actualDir.GetZ(),expectedDir.GetZ());
+
+  EXPECT_EQ(drone->GetId(),2);
+
+  EXPECT_EQ(drone->GetSpeed(),4)
 }
