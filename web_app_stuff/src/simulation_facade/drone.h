64dde0063365f6067a783fd16880cf38e051f7dd
Julia Schechter <schec070@umn.edu>
2021-12-03 00:31:28 -0600

    Probably finished the merge, still working on changes and bugfixes

--- web_app_stuff/src/simulation_facade/drone.h
+++ web_app_stuff/src/simulation_facade/drone.h
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-#ifndef DRONE_H_
-#define DRONE_H_
-
-#include "movement_strategy.h"
-#include "entity.h"
-
-
-class Drone : public Entity{
-public:
-	Drone();
-
-	Drone(Point3 newPosition, Vector3 newDirection, Vector3 newVelocity, float newTime, int newTravelNode, int newCurrentNode);
-
-	Drone(const Drone &old);
-
-	void Move();
-	Point3 TakePicture();
-
-	void Update(float dt);
-
-private:
-	// Point3 position;
-	// Vector3 direction;
-	// Vector3 velocity;
-	// float time;
-	Point3 robotFound;
-	int travelNode; // keeps track of the drone's target location along the edge of the map, increments when it's at a recharge station
-	int currentNode; // for the drone's patrol, where it's currently moving to
-};
-
-#endif
-=======
 /**
  * @file drone.h
  *
@@ -44,6 +11,7 @@ private:
  ******************************************************************************/
 #include "entity.h"
 #include "battery.h"
+#include "movement_strategy.h"
 
 /**
  * @brief The main class used for drones
@@ -71,7 +39,7 @@ public:
 	 *
 	 * @return a drone object
 	 */
-	Drone(const Drone& old);
+	Drone(const Drone &old);
 
 	// ~Drone();
 
@@ -91,8 +59,12 @@ public:
 	void TakePicture();
 
 	void Update(float dt);
-        Battery battery;  
+    Battery battery;
+
+private:
+	Point3 robotFound;
+	int travelNode; // keeps track of the drone's target location along the edge of the map, increments when it's at a recharge station
+	int currentNode; // for the drone's patrol, where it's currently moving to
 };
 
 #endif
->>>>>>> dev
