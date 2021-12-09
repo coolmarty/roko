8ff5f726a7e2a49f5750a9e916d0f790444452cf
subra231 <subra231@umn.edu>
2021-12-09 01:53:06 -0600

    Added empty image to bool blobdetect

--- src/simulation_facade/image_processing.cc
+++ src/simulation_facade/image_processing.cc
@@ -4,9 +4,11 @@
 
 bool ImageProcessing::BlobCheck(std::string fname) {
 	Image in_img = Image(fname);
+	Image out_img = Image(in_img.GetWidth(), in_img.GetHeight());
 	std::vector<Image *> input;
 	std::vector<Image *> output;
 	input.push_back(&in_img);
+	output.push_back(&out_img);
 
 	BlobDetect filter;
 	return filter.check(input, output);
@@ -14,9 +16,9 @@ bool ImageProcessing::BlobCheck(std::string fname) {
 
 float ImageProcessing::BlobGetDistance(std::string color_name, std::string depth_name) {
 	Image color_img = Image(color_name);
-	Image depth_img = Image(depth_name);
+	DirectionImage depth_img = DirectionImage(depth_name);
 	std::vector<Image*> color_vec;
-	std::vector<Image*> depth_vec;
+	std::vector<DirectionImage*> depth_vec;
 	color_vec.push_back(&color_img);
 	depth_vec.push_back(&depth_img);
 
@@ -26,9 +28,9 @@ float ImageProcessing::BlobGetDistance(std::string color_name, std::string depth
 
 std::vector<float> ImageProcessing::BlobGetDirection(std::string color_name, std::string depth_name) {
 	Image color_img = Image(color_name);
-	Image depth_img = Image(depth_name);
+	DirectionImage depth_img = DirectionImage(depth_name);
 	std::vector<Image*> color_vec;
-	std::vector<Image*> depth_vec;
+	std::vector<DirectionImage*> depth_vec;
 	color_vec.push_back(&color_img);
 	depth_vec.push_back(&depth_img);
 
